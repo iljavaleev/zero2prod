de32bdf831c07aeced9a19bb48370a623753a642
iljavaleev <iljavaleev@gmail.com>
2026-05-10 18:47:24 +0500

    subs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, http::Error, web};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, 
+    dev::Server, web};
+use std::net::TcpListener;
 
 
 async fn greet(req: HttpRequest) -> impl Responder{
@@ -11,15 +13,20 @@ async fn health_check(_req: HttpRequest) -> impl Responder{
     HttpResponse::Ok().finish() // resp with empty body
 }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
-pub fn run() -> std::io::Result<Server> {
+
+pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| { // no params
         App::new()
         .route("/", web::get().to(greet))
         .route("/health_check", web::get().to(health_check))
-         .route("/{name}", web::get().to(greet))
+        .route("/subscriptions", web::post().to(subscribe))
+        .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
