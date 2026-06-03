2706ed362a83d44b3507a7dd310f1922f24c3fab
iljavaleev <iljavaleev@gmail.com>
2026-06-03 16:27:20 +0500

    obs

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> std::io::Result<Server>
     // Capture `connection` from the surrounding environment with move
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
